cbcecf04a9da44dfb26c83ecf177b4a3878051c3
Alina Kornilova <alinakornilova78@gmail.com>
2019-10-07 11:55:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,20 @@
-class Puppy
+lass Dog
+  attr_accessor :name
+
   @@all = []
- 
-  attr_accessor :dog
- 
-  def initialize(dog)
-    @dog = dog
+
+  def initialize(name)
+    @name = name
     @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each do |d|
+      puts d.name
+    end
   end
 end
